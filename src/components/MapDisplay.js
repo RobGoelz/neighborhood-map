655367682b868a4a59768b3968a482daf53d7aed
Rob Goelz <RobGoelz@gmail.com>
2018-10-31 11:57:32 -0400

    fix: remove unused style const

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -21,10 +21,6 @@ class MapDisplay extends Component {
 
 
   render = () => {
-    const style = {
-      width: '100%',
-      height: '100%',
-    }
 
     const center = {
       lat: this.props.lat,
