33fea3b194c08ac76d4b53373a39b7e4a43f51fc
Rob Goelz <RobGoelz@gmail.com>
2018-11-02 14:40:24 -0400

    refactor: modify Container to be used as customer loader/error handler

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,23 +1,38 @@
 import React, {Component} from 'react';
 
 class Container extends Component {
-  render () {
-    const style = {
-      width: '100vw',
-      height: '100vh'
-    }
+  state = {
+    show: false,
+    timeout: null
+  }
 
-    if (!this.props.loaded) {
-      return (<div>Loading...</div>);
-    }
+  componentDidMount = () => {
+    let timeout = window.setTimeout(this.showMessage, 1000);
+    this.setState({timeout});
+  }
+
+  componentWillUnmount = () => {
+    window.clearTimeout(this.state.timeout);
+  }
+
+  showMessage = () => {
+    this.setState({show: true});
+  }
+
+  render () {
     return (
-      <div style={style}>
-        <Map google={this.props.google} />
+      <div>
+        {this.state.show
+          ? (
+              <div>
+                <h1>Error loading map</h1>
+                <p>Could not load map due to a network error. Try again later.</p>
+              </div>
+          ) : (<div><h1>Loading...</h1></div>)
+        }
       </div>
     )
   }
 }
 
-export default GoogleApiComponent({
-  apiKey: ''
-})(Container)
+export default Container;
